b663acb4c26f5fdade5c1098437a43c9dadfb400
QiratMehmoodali <162498646+QiratMehmoodali@users.noreply.github.com>
2024-03-08 02:01:25 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+//Q2:
+//let personName="Qirat";
+//console.log('Hello',personName,"would you like to learn some typescript today");
+//Q3:
+let personName="Qirat";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
